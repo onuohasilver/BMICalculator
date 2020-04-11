e6efe883c4382e24026674f5da555b20fe3cdc07
onuohasilver <onuohasilver9@gmail.com>
2020-04-11 09:37:41 +0100

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    MaterialApp(
+      home:BmiCalculator()
+    ),
+  );
+}
+
+class BmiCalculator extends StatefulWidget {
+  @override
+  _BmiCalculatorState createState() => _BmiCalculatorState();
+}
+
+class _BmiCalculatorState extends State<BmiCalculator> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
